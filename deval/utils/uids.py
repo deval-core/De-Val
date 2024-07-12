10608ba46209c6b7106ab56f401ad8c175ad7193
deval-core <austnbell+deval@gmail.com>
2024-07-12 12:04:54 +0200

    logging

--- deval/utils/uids.py
+++ deval/utils/uids.py
@@ -24,11 +24,13 @@ def check_uid_availability(
     print(f"checking UID: {uid}")
     # Filter non serving axons.
     if not metagraph.axons[uid].is_serving:
+        print("not serving")
         bt.logging.debug(f"uid: {uid} is not serving")
         return False
 
     # Filter validator permit > 1024 stake.
     if metagraph.validator_permit[uid] and metagraph.S[uid] > vpermit_tao_limit:
+        print("stake filter")
         bt.logging.debug(
             f"uid: {uid} has vpermit and stake ({metagraph.S[uid]}) > {vpermit_tao_limit}"
         )
@@ -73,6 +75,7 @@ def get_random_uids(self, k: int, exclude: List[int] = None) -> torch.LongTensor
             coldkeys,
             ips,
         )
+        print(f"availability: {uid_is_available}")
         if not uid_is_available:
             continue
 

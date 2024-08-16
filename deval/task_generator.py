8ee2f03c510bfa197009c649ba2a987ee227355b
deval-core <austnbell+deval@gmail.com>
2024-08-15 19:15:42 -0500

    significantly simplify task registry

--- deval/task_generator.py
+++ deval/task_generator.py
@@ -1,58 +1,22 @@
 from deval.tasks import (
     Task,
-    HallucinationTask,
-    CompletenessTask,
-    AttributionTask,
-    RelevancyTask
 )
-from deval.tools import (
-    WikiDataset,
-    GenericDataset,
-    AttributionDataset
-)
-from deval.tasks import TasksEnum
+from deval.tasks import TasksEnum, TASKS
 
 
 def create_task(llm_pipeline, task_name: str) -> Task:
-    wiki_based_tasks = [TasksEnum.RELEVANCY.value]
-    generic_text_tasks = [TasksEnum.HALLUCINATION.value, TasksEnum.COMPLETENESS.value]
-    attribution_text_tasks = [TasksEnum.ATTRIBUTION.value]
     
-    if task_name in wiki_based_tasks:
-        dataset = WikiDataset()
-
-    elif task_name in generic_text_tasks:
-        dataset = GenericDataset()
-    
-    elif task_name in attribution_text_tasks:
-        dataset = AttributionDataset()
-    
-    if task_name == TasksEnum.HALLUCINATION.value:
-        task = HallucinationTask(
-            llm_pipeline=llm_pipeline, 
-            context=dataset.next(),
-        )
-
-    elif task_name == TasksEnum.COMPLETENESS.value:
-        task = CompletenessTask(
-            llm_pipeline=llm_pipeline, 
-            context=dataset.next(),
-        )
-    
-    elif task_name == TasksEnum.ATTRIBUTION.value:
-        task = AttributionTask(
-            llm_pipeline=llm_pipeline, 
-            context=dataset.next(),
-        )
-    
-    elif task_name == TasksEnum.RELEVANCY.value:
-        task = RelevancyTask(
-            llm_pipeline=llm_pipeline, 
-            context=dataset.next(),
-        )
-
-    else:
+    task_extract = TASKS.get(task_name, None)
+    if task_extract is None:
         raise ValueError(f"Task {task_name} not supported. Please choose a valid task")
+
+    task_function = task_extract['task_function']
+    dataset = task_extract['dataset']()
+
+    task = task_function(
+        llm_pipeline=llm_pipeline, 
+        context=dataset.next(),
+    )
 
     return task
 
--- deval/tasks/__init__.py
+++ deval/tasks/__init__.py
@@ -4,10 +4,28 @@ from .attribution import AttributionTask
 from .relevancy import RelevancyTask
 from .task import Task, TasksEnum
 
+from deval.tools import (
+    WikiDataset,
+    GenericDataset,
+    AttributionDataset
+)
 
+# To add a new task, add a new entry here with the relevant function and dataset
 TASKS = {
-    TasksEnum.HALLUCINATION.value: HallucinationTask,
-    TasksEnum.COMPLETENESS.value: CompletenessTask,
-    TasksEnum.ATTRIBUTION.value: AttributionTask,
-    TasksEnum.RELEVANCY.value: RelevancyTask
+    TasksEnum.RELEVANCY.value: {
+        "task_function": RelevancyTask,
+        "dataset": WikiDataset,
+    },
+    TasksEnum.HALLUCINATION.value: {
+        "task_function": HallucinationTask,
+        "dataset": GenericDataset,
+    },
+    TasksEnum.COMPLETENESS.value: {
+        "task_function": CompletenessTask,
+        "dataset": GenericDataset,
+    },
+    TasksEnum.ATTRIBUTION.value: {
+        "task_function": AttributionTask,
+        "dataset": AttributionDataset
+    }
 }

80e03fb2b7f7b0bdbe24f5445aa5820bd75d492f
deval-core <austnbell+deval@gmail.com>
2024-11-13 09:51:50 +0100

    only allow for every 100 epochs to set weights

--- deval/base/neuron.py
+++ deval/base/neuron.py
@@ -160,7 +160,9 @@ class BaseNeuron(ABC):
         ):
             return False
 
-        return True
+        return (
+            self.block - self.metagraph.last_update[self.uid]
+        ) > self.config.neuron.epoch_length
 
     def save_state(self):
         pass

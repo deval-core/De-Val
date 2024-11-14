b3dff9833fc9d295f9ef3d917f0764e1dc8eada0
deval-core <austnbell+deval@gmail.com>
2024-11-14 14:33:39 +0100

    require safetensor and commit dates prior to model submission

--- deval/model/model_state.py
+++ deval/model/model_state.py
@@ -103,6 +103,10 @@ class ModelState:
             bt.logging.info(f"Model size is too large - skipping evaluation")
             return False
 
+        if not self.last_commit_date or not self.last_safetensor_update:
+            bt.logging.info(f"Unable to get last commit date: {self.last_commit_date} or last safetensor update: {self.last_safetensor_update}")
+            return False
+
         if uid in top_incentive_uids:
             return True
 

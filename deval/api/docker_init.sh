33d14ff896d7f038e131d8a4bdf0d509681473bc
deval-core <austnbell+deval@gmail.com>
2025-01-28 13:01:51 +0100

    add tcp

--- deval/api/docker_init.sh
+++ deval/api/docker_init.sh
@@ -3,11 +3,11 @@
 # Allow loopback traffic
 iptables -A OUTPUT -o lo -j ACCEPT
 
-# Allow traffic to the Hugging Face endpoint (replace with actual IP/domain)
-iptables -A OUTPUT -p tcp -d https://huggingface.co --dport 443 -j ACCEPT
+# Allow traffic to the Hugging Face endpoint
+iptables -A OUTPUT -p tcp -d huggingface.co -m tcp --dport 443 -j ACCEPT
 
 # Allow traffic to the local API endpoint
-iptables -A OUTPUT -p tcp -d 0.0.0.0 --dport 8000 -j ACCEPT
+iptables -A OUTPUT -p tcp -d 0.0.0.0 -m tcp --dport 8000 -j ACCEPT
 
 # Block all other outgoing traffic
 iptables -A OUTPUT -j DROP

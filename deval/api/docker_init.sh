0da4371d58d34c781554d1b4c94a600d0f439a12
deval-core <austnbell+deval@gmail.com>
2025-02-02 17:01:50 +0100

    workable ip rules

--- deval/api/docker_init.sh
+++ deval/api/docker_init.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Allow loopback traffic
-iptables -A OUTPUT -o lo -j ACCEPT
-
-# Allow traffic to the Hugging Face endpoint
-iptables -A OUTPUT -p tcp -d huggingface.co -m tcp --dport 443 -j ACCEPT
-
-# Allow traffic to the local API endpoint
-iptables -A OUTPUT -p tcp -d 0.0.0.0 -m tcp --dport 8000 -j ACCEPT
-
-# Block all other outgoing traffic
 iptables -A OUTPUT -j DROP
+iptables -I OUTPUT -o lo -j ACCEPT
 
-# Execute the original command
+iptables -I OUTPUT -d 172.17.0.1 -j ACCEPT
+iptables -I OUTPUT -d 172.18.0.2 -j ACCEPT
+
+GATEWAY_IP=$(ip route | grep default | awk '{print $3}')
+iptables -I OUTPUT -d $GATEWAY_IP -j ACCEPT
+
+iptables -I OUTPUT -p tcp -d huggingface.co -m tcp --dport 443 -j ACCEPT
+iptables -I OUTPUT -p tcp -d cdn-lfs-us-1.hf.co -m tcp --dport 443 -j ACCEPT
+iptables -I OUTPUT -p tcp -d cdn-lfs-eu-1.hf.co -m tcp --dport 443 -j ACCEPT
+iptables -I OUTPUT -p tcp -d cdn-lfs.hf.co -m tcp --dport 443 -j ACCEPT
+
 exec "$@"

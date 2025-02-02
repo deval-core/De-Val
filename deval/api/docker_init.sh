12a35c1819e47f873a97ec2f239a8737e5b9852d
deval-core <austnbell+deval@gmail.com>
2025-02-02 18:52:16 +0100

    Rm internet access (#51)
    * first pass at iptables
    
    * update iptables and entrypoint
    
    * ensure iptables available
    
    * add tcp
    
    * enable iptables access
    
    * move entrypoint
    
    * workable ip rules
    
    * enable running as non-root user

---
+++ deval/api/docker_init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+iptables -A OUTPUT -j DROP
+iptables -I OUTPUT -o lo -j ACCEPT
+
+iptables -I OUTPUT -d 172.17.0.1 -j ACCEPT
+iptables -I OUTPUT -d 172.18.0.2 -j ACCEPT
+
+GATEWAY_IP=$(ip route | grep default | awk '{print $3}')
+iptables -I OUTPUT -d $GATEWAY_IP -j ACCEPT
+
+iptables -I OUTPUT -p tcp -d huggingface.co -m tcp --dport 443 -j ACCEPT
+iptables -I OUTPUT -p tcp -d cdn-lfs-us-1.hf.co -m tcp --dport 443 -j ACCEPT
+iptables -I OUTPUT -p tcp -d cdn-lfs-eu-1.hf.co -m tcp --dport 443 -j ACCEPT
+iptables -I OUTPUT -p tcp -d cdn-lfs.hf.co -m tcp --dport 443 -j ACCEPT
+
+exec gosu miner "$@"

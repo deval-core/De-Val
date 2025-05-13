ebe8494f2f7fe0bfcadf4416dbe577f0f3d7e575
deval-core <austnbell+deval@gmail.com>
2025-05-13 11:51:43 +0200

    init

--- deval/contest.py
+++ deval/contest.py
@@ -18,6 +18,7 @@ class DeValContest:
         self.reward_pipeline: RewardPipeline = reward_pipeline
         self.timeout: int = timeout
 
+        # insert
         self.tiers = {
             0 : 0.5,
             1 : 0.3,
